901013e6b2c50ab5a218aa3068a34697336d1aa7
aa425-aarohcharne <aaroh.charne@gmail.com>
2025-08-10 20:48:08 -0600

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,5 +1,7 @@
-wget https://github.com/aa425-aarohcharne/The-Aaroh-browser/releases/tag/v1
+wget wget https://github.com/aa425-aarohcharne/The-Aaroh-browser/releases/download/v1/the_aaroh_browser.deb
+
 sudo dpkg -i the_aaroh_browser.deb
+
 
 
 
